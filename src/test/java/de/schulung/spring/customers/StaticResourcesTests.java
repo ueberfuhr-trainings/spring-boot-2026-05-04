d752bb4fe26c247c5c28494d9095122adec774d5
Ralf Ueberfuhr <ralf.ueberfuhr@atvantage.com>
2026-05-05 08:46:41 +0200

    Add test for OpenAPI.yml and fix tests

--- src/test/java/de/schulung/spring/customers/StaticResourcesTests.java
+++ src/test/java/de/schulung/spring/customers/StaticResourcesTests.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,12 +24,22 @@ class StaticResourcesTests {
   void should_have_landing_page() throws Exception {
     mockMvc
       .perform(get("/"))
-      .andExpect(status().isOk())
-      .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+      .andExpect(status().isOk());
+    // mit MockMvc kein vollständiger Webserver -> greift nicht
+    // .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
   }
 
   /*
    * Das OpenAPI ist verfügbar unter /openapi.yml.
    */
+
+  @Test
+  void should_have_openapi_yml() throws Exception {
+    mockMvc
+      .perform(get("/openapi.yml"))
+      .andExpect(status().isOk());
+    // mit MockMvc kein vollständiger Webserver -> greift nicht
+    //.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_YAML));
+  }
 
 }
