592d795def0b846885ea5291bccbd456c4c7f92e
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2026-05-06 08:56:35 +0200

    Introduce CustomersAPI (tests and controller) (#3)

---
+++ src/main/java/de/schulung/spring/customers/CustomersController.java
@@ -0,0 +1,80 @@
+package de.schulung.spring.customers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomersController {
+
+  private final Map<UUID, Customer> customers = new ConcurrentHashMap<>();
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public Stream<Customer> getCustomers(
+    @RequestParam(value = "state", required = false)
+    String stateFilter
+  ) {
+    return customers
+      .values()
+      .stream()
+      .filter(customer -> stateFilter == null || customer.getState().equals(stateFilter));
+  }
+
+  @GetMapping(
+    path = "/{uuid}",
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public Customer findCustomerById(@PathVariable UUID uuid) {
+    if (!customers.containsKey(uuid)) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    return customers.get(uuid);
+  }
+
+  @PostMapping
+  // @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+
+    customer.setUuid(UUID.randomUUID());
+    customers.put(customer.getUuid(), customer);
+
+    final var location = ServletUriComponentsBuilder
+      .fromCurrentRequest()
+      .path("/{uuid}")
+      .buildAndExpand(customer.getUuid())
+      .toUri();
+
+    return ResponseEntity
+      .created(location)
+      .body(customer);
+
+  }
+
+  @DeleteMapping("/{uuid}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void deleteCustomer(@PathVariable UUID uuid) {
+    if (!customers.containsKey(uuid)) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+    customers.remove(uuid);
+  }
+
+
+}
---
+++ src/test/java/de/schulung/spring/customers/CustomersApiTests.java
@@ -0,0 +1,340 @@
+package de.schulung.spring.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CustomersApiTests {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  /*
+   * GET /customers -> 200, Content-Type: application/json, Liste von Customers
+   */
+
+  @Test
+  void shouldGetCustomersReturn200() throws Exception {
+    mockMvc
+      .perform(
+        get("/customers")
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$").isArray());
+  }
+
+  /*
+   * GET /customers mit Accept: application/xml -> 406 Not Acceptable
+   */
+  @Test
+  void shouldGetCustomersAsXmlReturnNotAcceptable() throws Exception {
+    mockMvc
+      .perform(
+        get("/customers")
+          .accept(MediaType.APPLICATION_XML)
+      )
+      .andExpect(status().isNotAcceptable());
+  }
+
+  /*
+   * POST /customers mit
+   * {
+   *   "name": "Tom Mayer",
+   *   "birthdate": "2026-05-05",
+   *   "state": "active"
+   * }
+   * -> 201 Created, application/json, JSON prüfen?, Location: /customers/{uuid} (existiert)
+   */
+  @Test
+  void shouldCreateCustomerReturn201() throws Exception {
+    mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.name").value("Tom Mayer"))
+      .andExpect(jsonPath("$.birthdate").value("2026-05-05"))
+      .andExpect(jsonPath("$.state").value("active"))
+      .andExpect(jsonPath("$.uuid").isNotEmpty())
+      // assert that Location header is set and contains an absolute URL
+      .andExpect(result -> {
+        final var locationHeader = result
+          .getResponse()
+          .getHeader("Location");
+        assertThat(locationHeader)
+          .isNotNull();
+        final var uri = URI.create(locationHeader);
+        assertThat(uri.isAbsolute()).isTrue();
+      });
+
+  }
+
+  @Test
+  void shouldSetDefaultActiveStateOnCreateCustomer() throws Exception {
+    mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.state").value("active"));
+
+  }
+
+  @Test
+  void shouldAllowReadingSingleCustomerByUuidAfterCreation() throws Exception {
+    // setup
+    var responseBody = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+    var uuid = new ObjectMapper()
+      .readTree(responseBody)
+      .path("uuid")
+      .asString();
+
+    // Test
+    mockMvc
+      .perform(get("/customers/{uuid}", uuid))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.uuid").value(uuid))
+      .andExpect(jsonPath("$.name").value("Tom Mayer"))
+      .andExpect(jsonPath("$.birthdate").value("2026-05-05"))
+      .andExpect(jsonPath("$.state").value("active"));
+
+  }
+
+  @Test
+  void shouldAllowReadingSingleCustomerByLocationHeaderAfterCreation() throws Exception {
+    // setup
+    var locationHeader = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getHeader("Location");
+
+    assertThat(locationHeader)
+      .isNotNull();
+
+    // Test
+    mockMvc
+      .perform(get(locationHeader))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$.uuid").isNotEmpty())
+      .andExpect(jsonPath("$.name").value("Tom Mayer"))
+      .andExpect(jsonPath("$.birthdate").value("2026-05-05"))
+      .andExpect(jsonPath("$.state").value("active"));
+
+  }
+
+  @Test
+  void shouldFindCustomerInAllCustomersAfterCreation() throws Exception {
+    // setup
+    var responseBody = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+    var uuid = new ObjectMapper()
+      .readTree(responseBody)
+      .path("uuid")
+      .asString();
+
+    // Test
+    mockMvc
+      .perform(get("/customers"))
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$[?(@.uuid == '%s')]".formatted(uuid)).exists());
+
+  }
+
+  @Test
+  void shouldFindCustomerInAllCustomersWithStateAfterCreation() throws Exception {
+    // setup
+    var responseBody = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+    var uuid = new ObjectMapper()
+      .readTree(responseBody)
+      .path("uuid")
+      .asString();
+
+    // Test
+    mockMvc
+      .perform(
+        get("/customers")
+          .param("state", "active")
+      )
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$[?(@.uuid == '%s')]".formatted(uuid)).exists());
+    mockMvc
+      .perform(
+        get("/customers")
+          .param("state", "locked")
+      )
+      .andExpect(status().isOk())
+      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+      .andExpect(jsonPath("$[?(@.uuid == '%s')]".formatted(uuid)).doesNotExist());
+
+  }
+
+  @Test
+  void shouldNotCreateCustomerWithXmlContentType() throws Exception {
+    mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_XML)
+          .content("<customer/>")
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isUnsupportedMediaType());
+  }
+
+  @Test
+  void shouldNotCreateCustomerWithInvalidAccept() throws Exception {
+    mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_XML)
+      )
+      .andExpect(status().isNotAcceptable());
+    // TODO Customer is not created?
+  }
+
+  @Test
+  void shouldDeleteCustomerAndCustomerShouldNotExistAfterDeletion() throws Exception {
+    // setup
+    var responseBody = mockMvc
+      .perform(
+        post("/customers")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content("""
+            {
+              "name": "Tom Mayer",
+              "birthdate": "2026-05-05",
+              "state": "active"
+            }
+            """)
+          .accept(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated())
+      .andReturn()
+      .getResponse()
+      .getContentAsString();
+    var uuid = new ObjectMapper()
+      .readTree(responseBody)
+      .path("uuid")
+      .asString();
+
+    // Test
+    mockMvc
+      .perform(delete("/customers/{uuid}", uuid))
+      .andExpect(status().isNoContent());
+    mockMvc
+      .perform(get("/customers/{uuid}", uuid))
+      .andExpect(status().isNotFound());
+    mockMvc
+      .perform(delete("/customers/{uuid}", uuid))
+      .andExpect(status().isNotFound());
+  }
+
+}
